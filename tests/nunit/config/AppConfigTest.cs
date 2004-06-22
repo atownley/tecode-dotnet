afb1639c92487fff8d4a0672eadc6901f1b8a02d
atownley <atownley>
2004-06-22 11:57:08 +0000

    Added changes to test the new use of resolution strategy objects.

--- tests/nunit/config/AppConfigTest.cs
+++ tests/nunit/config/AppConfigTest.cs
@@ -50,7 +50,7 @@ namespace TownleyEnterprises.Config {
 /// <summary>
 ///   Tests for the AppConfig class.
 /// </summary>  
-/// <version>$Id: AppConfigTest.cs,v 1.1 2004/06/21 15:53:09 atownley Exp $</version>
+/// <version>$Id: AppConfigTest.cs,v 1.2 2004/06/22 11:57:08 atownley Exp $</version>
 /// <author><a href="mailto:adz1092@netscape.net">Andrew S. Townley</a></author>
 //////////////////////////////////////////////////////////////////////
 
@@ -61,7 +61,7 @@ public sealed class AppConfigTest
 	public void InitConfig()
 	{
 		config = new AppConfig("actest");
-		Hashtable ht1 = new Hashtable();
+		ht1 = new Hashtable();
 		ht1["key1"] = "val1";
 		ht1["key2"] = "val2";
 		ht1["key3"] = "val3";
@@ -70,13 +70,22 @@ public sealed class AppConfigTest
 			new DictionaryConfigSupplier("actest", ht1);
 		config.RegisterConfigSupplier(cs1);
 
-		Hashtable ht2 = new Hashtable();
+		ht2 = new Hashtable();
 		ht2["key2"] = "override";
-		
+	
 		IConfigSupplier cs2 = 
 			new DictionaryConfigSupplier("actest", ht2);
 		config.RegisterConfigSupplier(cs2);
 
+		ht3 = new Hashtable();
+		ht3["eggs"] = "green";
+		ht3["ham"] = "true";
+		ht3["key3"] = "true";
+		
+		IConfigSupplier cs3 = 
+			new DictionaryConfigSupplier("actest", ht3);
+		config.RegisterConfigSupplier(cs3);
+		
 //Console.WriteLine("cs1 hash code:  {0}", cs1.GetHashCode());
 //Console.WriteLine("cs2 hash code:  {0}", cs2.GetHashCode());
 	}
@@ -94,6 +103,12 @@ public sealed class AppConfigTest
 	}
 
 	[Test]
+	public void VerifyOverrideProperty2ndLevel()
+	{
+		Assert.AreEqual("true", config["key3"]);
+	}
+
+	[Test]
 	public void VerifyIgnoreSettings()
 	{
 		Hashtable ht = new Hashtable();
@@ -105,6 +120,62 @@ public sealed class AppConfigTest
 		Assert.IsNull(config["ht"], "value should be ignored because application name is not the same");
 	}
 
+	[Test]
+	public void VerifyEmptySettings()
+	{
+		Hashtable ht = new Hashtable();
+		ht["key1"] = null;
+		ht["key2"] = null;
+		ht["key3"] = null;
+		
+		config.RegisterConfigSupplier(
+			new DictionaryConfigSupplier("actest", ht));
+		
+		Assert.IsNull(config["key1"], "value should override existing setting.");
+		Assert.IsNull(config["key2"], "value should override existing setting.");
+		Assert.IsNull(config["key3"], "value should override existing setting.");
+	}
+
+	[Test]
+	public void VerifyBasicPropertySave()
+	{
+		config["key1"] = "new value";
+		Assert.AreEqual("new value", ht1["key1"]);
+	}
+
+	[Test]
+	public void VerifyOverridePropertySave()
+	{
+		config["key2"] = "new value";
+		Assert.AreEqual("new value", ht2["key2"]);
+	}
+
+	[Test]
+	public void VerifyWriteCapturingResolver()
+	{
+		AppConfig ac = new AppConfig("actest");
+		Hashtable hash = new Hashtable();
+		ac.TemplateResolver = new WriteCaptureConfigResolver(
+			new DictionaryConfigSupplier("actest", hash));
+
+		// cheat and use the already initialized values
+		ac.RegisterConfigSupplier(config);
+
+		ac["key1"] = "write capture1";
+		ac["key2"] = "write capture2";
+		Assert.AreEqual("write capture1", ac["key1"]);
+		Assert.AreEqual("write capture2", ac["key2"]);
+		Assert.AreEqual("write capture1", hash["key1"]);
+		Assert.AreEqual("write capture2", hash["key2"]);
+		Assert.AreEqual("val1", config["key1"]);
+		Assert.AreEqual("override", config["key2"]);
+		Assert.AreEqual("val1", ht1["key1"]);
+		Assert.AreEqual("override", ht2["key2"]);
+	}
+
+	private Hashtable	ht1;
+	private Hashtable	ht2;
+	private Hashtable	ht3;
 	private AppConfig	config;
 }
 
