851588db76f22ad475e125b27ded5a345e26e431
atownley <atownley>
2004-07-23 05:51:00 +0000

    Fix example based on changes in the constructors of the command option constraints.

--- examples/command/feather3.cs
+++ examples/command/feather3.cs
@@ -50,7 +50,7 @@ using TownleyEnterprises.Command;
 ///   illustrate the proper use of the CommandParser and the Command
 ///   namespace.
 /// </summary>
-/// <version>$Id: feather3.cs,v 1.2 2004/07/20 14:14:16 atownley Exp $</version>
+/// <version>$Id: feather3.cs,v 1.3 2004/07/23 05:51:00 atownley Exp $</version>
 /// <author><a href="mailto:adz1092@netscape.net">Andrew S. Townley</a></author>
 //////////////////////////////////////////////////////////////////////
 
@@ -96,9 +96,9 @@ public class feather
 		// initialize the delegate
 		_create.OnExecute += CreateArchive;
 
-		parser.AddConstraint(new MutexOptionConstraint(_create,-500, _extract));
-		parser.AddConstraint(new RequiresAnyOptionConstraint(_file, -501, new CommandOption[] { _create, _extract }));
-		parser.AddConstraint(new RequiresAnyOptionConstraint(_xclude, -502, new CommandOption[] { _create, _extract }));
+		parser.AddConstraint(new MutexOptionConstraint(-500, _create, _extract));
+		parser.AddConstraint(new RequiresAnyOptionConstraint(-501, _file, new CommandOption[] { _create, _extract }));
+		parser.AddConstraint(new RequiresAnyOptionConstraint(-502, _xclude, new CommandOption[] { _create, _extract }));
 
 		parser.ExecuteCommands(this);
 	}
